313294f0cb54c48e3fffcaa928d7e75aa1d95c8d
raobathula970-png <raobathula970@gmail.com>
2026-06-26 21:38:51 +0530

    Update assigment2.ts

--- type-scripttraining/assignments/assigment2.ts
+++ type-scripttraining/assignments/assigment2.ts
@@ -1,4 +1,5 @@
 
+
 let customerName : string= "John Doe";
 let creditScore : number = 720;
 let income :number= 55000.0;
@@ -11,35 +12,29 @@ if (creditScore > 750){
     console.log("John Doe is eligible: credit score is above 750, the loan is automatically approved. ");
 }
 else if (creditScore >= 650 && creditScore <= 750){
-    console.log(" John Doe credit score is between 650 and 750, additional checks are performed.");
-
+console.log(" John Doe credit score is between 650 and 750, additional checks are performed.");
 }
-else if(creditScore < 650){
-    console.log(` ${customerName} credit score is below 650, the loan is denied.`);
-}
-
- if (income >= 50000){
-
-    console.log(` ${customerName} income must be at least $50,000
-for the loan to be considered.`);
-
+else if (creditScore < 650){
+console.log(` ${customerName} credit score is below 650, the loan is denied.`);
 }
 
-if (isEmployed = true){
-
-    console.log(` ${customerName}  income is at least 50,000, the system checks whether the customer is
-employed.`)
-}
-else if (isEmployed = false){
-
-    console.log(` ${customerName} is unemployed, the loan is denied.`);
-}
-
-if (debtToIncomeRatio < 40){
-console.log(` ${customerName} is employed, the system checks the debt-to-income (DTI) ratio.`);
-console.log(` ${customerName}DTI ratio is less than 40%, the loan is approved.`);
-}
-else if (debtToIncomeRatio > 40){
-
-    console.log(`${customerName} DTI ratio is 40% or greater, the loan is denied.`);
-}
+    if (income >= 50000) {
+            // If income is at least $50,000, check employment status
+            if (isEmployed = true) {
+                // Step 3: Check debt-to-income ratio
+                if (debtToIncomeRatio < 40.0) {
+                    // If DTI ratio is less than 40%, approve the loan
+                    console.log("Loan Approved: Meets all criteria.");
+                } else {
+                    // If DTI is 40% or greater, deny the loan
+                    console.log("Loan Denied: Debt-to-Income ratio is too high.");
+                }
+            } else {
+                // If the customer is unemployed, deny the loan
+                console.log("Loan Denied: Customer is not employed.");
+            }
+        } else {
+            // If income is less than $50,000, deny the loan
+            console.log("Loan Denied: Income is less than required.");
+        }
+    
